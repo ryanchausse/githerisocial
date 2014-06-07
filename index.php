8f22582dd70f363c9a85678f78cbee9591d9e634
Chausse <Chausse@BlackKeys>
2014-06-08 08:23:13 +1000

    First commit to git

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Githeri.com Copyright 2013. All Rights Reserved.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Githeri - Karibu!</title>
+    </head>
+    <body>
+        <?php
+        echo "derp";
+        // put your code here
+        ?>
+    </body>
+</html>
