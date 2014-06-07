b5e09baeda9fcbcd9a99cc39f3ba2f5337a9e39a
Chausse <Chausse@BlackKeys>
2014-06-08 09:06:01 +1000

    Third commit

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ Githeri.com Copyright 2013. All Rights Reserved.
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Githeri - Karibu!</title>
     </head>
     <body>
         <?php
         echo "derp";
-        // put your code here
         ?>
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
